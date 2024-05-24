0a9c01ed0459c414f49403ca38e238f2c7e206b9
E1B01 <u11202201@o365.ttu.edu.tw>
2024-05-25 00:09:32 +0800

    second version 完成功能a,b

--- HW4.c
+++ HW4.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct data{
+	char name[10];
+	char id[7];
+	int math;
+	int physics;
+	int english;
+}student[10];
+
 int exit_system(){
 	printf("Continue? (y/n): ");
     while(1){
@@ -20,6 +28,86 @@ int exit_system(){
             printf("Continue? (y/n): ");
 		}
     }
+}
+
+void input_grade(int nn){
+
+		int i;
+		
+			for(i=0 ; i < nn ; i++){
+				printf("\n");
+				printf("please input student name:");
+				fflush(stdin);							/*clear input buffer*/
+				scanf("%s",student[i].name);
+				printf("please input student id:");
+				fflush(stdin);							/*clear input buffer*/
+				scanf("%s",student[i].id);
+				for(;;){
+					if(strlen(student[i].id) == 6){
+						break;	
+					}else{
+						printf("error\n");
+						printf("please input student id again:");
+						fflush(stdin);							/*clear input buffer*/
+						scanf("%s",student[i].id);
+					}	
+				}
+				printf("please input student math grade:");
+				fflush(stdin);							/*clear input buffer*/
+				scanf("%d",&student[i].math);
+				for(;;){
+					if(student[i].math >= 0 && student[i].math <= 100){
+						break;	
+					}else{
+						printf("error\n");
+						printf("please input student math grade again:");
+						fflush(stdin);							/*clear input buffer*/
+						scanf("%d",&student[i].math);
+					}	
+				}
+				printf("please input student physics grade:");
+				fflush(stdin);							/*clear input buffer*/
+				scanf("%d",&student[i].physics);
+				for(;;){
+					if(student[i].physics >= 0 && student[i].physics <= 100){
+						break;	
+					}else{
+						printf("error\n");
+						printf("please input student physics grade again:");
+						fflush(stdin);							/*clear input buffer*/
+						scanf("%d",&student[i].physics);
+					}	
+				}
+				printf("please input student english grade:");
+				fflush(stdin);							/*clear input buffer*/
+				scanf("%d",&student[i].english);
+				for(;;){
+					if(student[i].english >= 0 && student[i].english <= 100){
+						break;	
+					}else{
+						printf("error\n");
+						printf("please input student english grade again:");
+						fflush(stdin);							/*clear input buffer*/
+						scanf("%d",&student[i].english);
+					}	
+				}
+				printf("\n");
+				printf("\n");
+			}system("pause");                        /*pause the screen*/
+    		system("cls");                           /*clear the screen*/
+		
+	
+}
+
+void print_data(int nn){
+	int i;
+	float avg;
+	printf("name              id        math     physics     english     average\n");
+	for(i=0 ; i < nn ; i++){
+		avg=(float)(student[i].math+student[i].physics+student[i].english)/3;
+		printf("%-10s      %-7s      %2d        %2d         %2d          %.1f\n",student[i].name,student[i].id,student[i].math,student[i].physics,student[i].english,avg);
+	}system("pause");                        /*pause the screen*/
+    system("cls");                           /*clear the screen*/
 }
 
 int main() {
@@ -83,10 +171,22 @@ int main() {
     	scanf("%s", choice);
     	system("pause");                        /*pause the screen*/
     	system("cls");                           /*clear the screen*/
+    	
+    	int n;
     
     	if(strcmp(choice,a)==0){
-    	
+    		for(;;){
+    			printf("please input n(5~10) which is type of integer:");
+    			fflush(stdin);							/*clear input buffer*/
+				scanf("%d",&n);
+				if(n>=2 && n<=10){
+    				input_grade(n);
+    				break;
+    			}
+			}	
+    		
 		}else if(strcmp(choice,b)==0){
+			print_data(n);
 		
 		}else if(strcmp(choice,c)==0){
 		

0bd4c090ea46c00e758b95741f470a14d086545f
E1B01 <u11202201@o365.ttu.edu.tw>
2024-05-25 12:33:59 +0800

    心得:   這次的作業我覺得比上次的簡單多了，這次我覺得比較難的是從大排到小，因為這次平均成績是浮點數，有精確度的問題，沒辦法讓數字直接從100跑到0去排序，所以我只好用泡泡排序法，會想到這個是因為，在七樓讀書的時候，剛好隔壁班的他在整理程式設計的筆記，我就看到了這個方法，其他的我覺得沒有很難，相較上一次真的簡單很多。

--- HW4.c
+++ HW4.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct data{
+struct data{                      //declare a structure to save data
 	char name[10];
 	char id[7];
 	int math;
@@ -11,21 +11,19 @@ struct data{
 	float average;
 }student[10];
 
-float average[10],average1[10];
-
-int exit_system(){
+int exit_system(){                 //function e
 	printf("Continue? (y/n): ");
     while(1){
 
         char _continue[10],y[2]="y",n[2]="n";
         fflush(stdin);							/*clear input buffer*/
         scanf("%s", _continue);
-        if (strcmp(_continue,y)==0) {
+        if (strcmp(_continue,y)==0) {            //determine whether input 'y'
             system("pause");                        /*pause the screen*/
     		system("cls");                           /*clear the screen*/
     		return 1;
     		break;
-        }else if (strcmp(_continue,n)==0) {
+        }else if (strcmp(_continue,n)==0) {    //determinr whether input 'n'
             return 0;
         }else{
             printf("Continue? (y/n): ");
@@ -33,10 +31,10 @@ int exit_system(){
     }
 }
 
-void input_grade(int nn){
+void input_grade(int nn){                        //input student grade
 
 		int i;
-		
+
 			for(i=0 ; i < nn ; i++){
 				printf("\n");
 				printf("please input student name:");
@@ -45,7 +43,7 @@ void input_grade(int nn){
 				printf("please input student id:");
 				fflush(stdin);							/*clear input buffer*/
 				scanf("%s",student[i].id);
-				for(;;){
+				for(;;){                  //determine the type of student id
 					if(strlen(student[i].id) == 6){
 						break;	
 					}else{
@@ -58,7 +56,7 @@ void input_grade(int nn){
 				printf("please input student math grade:");
 				fflush(stdin);							/*clear input buffer*/
 				scanf("%d",&student[i].math);
-				for(;;){
+				for(;;){                 //determine the type of math grade
 					if(student[i].math >= 0 && student[i].math <= 100){
 						break;	
 					}else{
@@ -71,7 +69,7 @@ void input_grade(int nn){
 				printf("please input student physics grade:");
 				fflush(stdin);							/*clear input buffer*/
 				scanf("%d",&student[i].physics);
-				for(;;){
+				for(;;){                      //determine the type of physics grade
 					if(student[i].physics >= 0 && student[i].physics <= 100){
 						break;	
 					}else{
@@ -84,7 +82,7 @@ void input_grade(int nn){
 				printf("please input student english grade:");
 				fflush(stdin);							/*clear input buffer*/
 				scanf("%d",&student[i].english);
-				for(;;){
+				for(;;){                            //determine the type of english grade
 					if(student[i].english >= 0 && student[i].english <= 100){
 						break;	
 					}else{
@@ -98,15 +96,13 @@ void input_grade(int nn){
 				printf("\n");
 			}system("pause");                        /*pause the screen*/
     		system("cls");                           /*clear the screen*/
-		
-	
 }
 
-void print_data(int nn){
+void print_data(int nn){                    //print student data
 	int i;
 	float avg;
 	printf("name              id        math     physics     english     average\n");
-	for(i=0 ; i < nn ; i++){
+	for(i=0 ; i < nn ; i++){                  //input average into data
 		avg=(float)(student[i].math+student[i].physics+student[i].english)/3;
 		student[i].average=avg;
 		printf("%-10s      %-7s      %2d        %2d         %2d          %.1f\n",student[i].name,student[i].id,student[i].math,student[i].physics,student[i].english,student[i].average);
@@ -114,14 +110,14 @@ void print_data(int nn){
     system("cls");                           /*clear the screen*/
 }
 
-void print_high_to_low(int nnn){
+void print_high_to_low(int nnn){                 //print student data from high to low
 	int i,j;
 	float avg;
 	struct data box;
-	for(i=0 ; i < nnn ; i++){
+	for(i=0 ; i < nnn ; i++){                     //input average into data
 		avg=(float)(student[i].math+student[i].physics+student[i].english)/3;
 		student[i].average=avg;
-	}for(i=0 ; i < nnn-1 ; i++){
+	}for(i=0 ; i < nnn-1 ; i++){                 //use bubble sort to print student data from high to low
 		for(j=0 ; j < (nnn-1)-i ; j++){
 			if(student[j].average < student[j+1].average){
 				box = student[j];
@@ -130,18 +126,18 @@ void print_high_to_low(int nnn){
 			}
 		}
 	}printf("name              id        math     physics     english     average\n");
-	for(i=0 ; i < nnn ; i++){
+	for(i=0 ; i < nnn ; i++){                   //print student data
 		printf("%-10s      %-7s      %2d        %2d         %2d          %.1f\n",student[i].name,student[i].id,student[i].math,student[i].physics,student[i].english,student[i].average);
 	}system("pause");                        /*pause the screen*/
     system("cls");                           /*clear the screen*/
 }
 
-void search_student(){
+void search_student(){                        //search student
 	char search[10];
 	int i,nobody=0;
 	printf("please input the student you want to search:");
 	scanf("%s",search);
-	for(i=0 ; i < 10 ; i++){
+	for(i=0 ; i < 10 ; i++){                     //determine whether student name is exist
 		if(strcmp(student[i].name,search)==0){
 			printf("%-10s      %-7s      %2d        %2d         %2d          %.1f\n",student[i].name,student[i].id,student[i].math,student[i].physics,student[i].english,student[i].average);
 			nobody++;
@@ -216,27 +212,27 @@ int main() {
     	
     	int n;
     
-    	if(strcmp(choice,a)==0){
-    		for(;;){
+    	if(strcmp(choice,a)==0){                 //determine 'a'
+    		for(;;){                            //determine the input number
     			printf("please input n(5~10) which is type of integer:");
     			fflush(stdin);							/*clear input buffer*/
 				scanf("%d",&n);
 				if(n>=5 && n<=10){
-    				input_grade(n);
+    				input_grade(n);             //call function input_grade
     				break;
     			}
 			}	
     		
-		}else if(strcmp(choice,b)==0){
-			print_data(n);
+		}else if(strcmp(choice,b)==0){             //determine 'b'
+			print_data(n);                         //call function print_data
 		
-		}else if(strcmp(choice,c)==0){
-			search_student();
-		}else if(strcmp(choice,d)==0){
-			print_high_to_low(n);
-		}else if(strcmp(choice,e)==0){
+		}else if(strcmp(choice,c)==0){              //determine 'c'
+			search_student();                       //call function search_student
+		}else if(strcmp(choice,d)==0){              //determine 'd'
+			print_high_to_low(n);                    //call function print_high_to_low
+		}else if(strcmp(choice,e)==0){               //determine 'e'
 			int re;
-			re = exit_system();
+			re = exit_system();                      //call function exit_system and return 0 or 1
 			if(re==0){
 				return 0;
 			}
